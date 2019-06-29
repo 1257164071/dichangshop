20d948a7a3eb9c0729074504eb9ddf67cc44f97d
NightDays <1257164071@qq.com>
2019-06-29 16:00:41 +0800

    php版本升级

--- thinkphp/library/think/template/taglib/Cx.php
+++ thinkphp/library/think/template/taglib/Cx.php
@@ -145,7 +145,7 @@ foreach($result as $'.$key.'=>$'.$item.'):
     if(I("get.edit_ad") && $'.$item.'['."'not_adv'".'] == 0 )
     {
         $'.$item.'['."'style'".'] = "filter:alpha(opacity=50); -moz-opacity:0.5; -khtml-opacity: 0.5; opacity: 0.5"; // 广告半透明的样式
-        $'.$item.'['."'ad_link'".'] = "/index.php?m=Admin&c=Ad&a=ad&act=edit&ad_id=$'.$item.'['."'ad_id'".']";        
+        $'.$item.'['."'ad_link'".'] = "/index.php?m=Admin&c=Ad&a=ad&act=edit&ad_id=$".$item[\'ad_id\'];        
         $'.$item.'['."'title'".'] = $ad_position[$'.$item.'['."'pid'".']]['."'position_name'".']."===".$'.$item.'['."'ad_name'".'];
         $'.$item.'['."'target'".'] = 0;
     }
