4b6fd1e2934a76eb622a38ba9703281ba9b1e734
NightDays <1257164071@qq.com>
2019-10-11 17:55:06 +0800

    'f'

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    'hostname'       => 'localhost',
+    'hostname'       => '47.75.42.164',
     // 数据库名
     'database'       => 'demo_345',
     // 用户名
